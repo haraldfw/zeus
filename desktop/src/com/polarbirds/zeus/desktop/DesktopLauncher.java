36020c641c8639c6baf84c457b587eba8374160d
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-01-27 16:00:15 +0100

    Fixed application size

--- desktop/src/com/polarbirds/zeus/desktop/DesktopLauncher.java
+++ desktop/src/com/polarbirds/zeus/desktop/DesktopLauncher.java
@@ -1,7 +1,7 @@
 package com.polarbirds.zeus.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.polarbirds.zeus.ZeusGame;
 import com.polarbirds.zeus.net.UDPServer;
 
@@ -10,6 +10,8 @@ public class DesktopLauncher {
   public static void main(String[] args) {
     new Thread(new UDPServer(true)).start();
     LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    config.width = ZeusGame.PIXELS_PER_TILESIDE * ZeusGame.X_TILES;
+    config.height = ZeusGame.PIXELS_PER_TILESIDE * ZeusGame.Y_TILES;
     config.foregroundFPS = 60;
     config.backgroundFPS = 60;
     config.useGL30 = true;
