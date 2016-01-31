717130ee9faa7183892de6426cb87e502634761d
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-01-31 02:36:47 +0100

    World-class for wrapping data on the world

---
+++ core/src/com/polarbirds/zeus/world/generator/World.java
@@ -0,0 +1,50 @@
+package com.polarbirds.zeus.world.generator;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.polarbirds.zeus.world.Tile;
+
+import java.util.HashMap;
+
+/**
+ * Created by Harald on 30.01.2016.
+ */
+public class World {
+
+  public Vector2 spawn;
+  public Tile[][] tiles;
+  private HashMap<Tile.TileGraphic, Texture> textures;
+
+  World(Vector2 spawn, Tile[][] tiles, HashMap<Tile.TileGraphic, Texture> textures) {
+    this.spawn = spawn;
+    this.tiles = tiles;
+    this.textures = textures;
+  }
+
+  public void drawFloor(SpriteBatch batch) {
+    for (int x = 0; x < tiles.length; x++) {
+      for (int y = 0; y < tiles[0].length; y++) {
+        Tile tile = tiles[x][y];
+        if (tile.graphic == Tile.TileGraphic.FLOOR_BASIC) {
+          batch.draw(textures.get(tile.graphic), x, y);
+        }
+      }
+    }
+  }
+
+  public void drawWalls(SpriteBatch batch) {
+    for (int x = 0; x < tiles.length; x++) {
+      for (int y = 0; y < tiles[0].length; y++) {
+        Tile tile = tiles[x][y];
+        if(tile.graphic == Tile.TileGraphic.WALL_BASIC) {
+
+        }
+      }
+    }
+  }
+
+  public enum WorldType {
+    HUB;
+  }
+}
